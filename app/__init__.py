51c7c1c4240fb8e464131ae8f09601a7381a3304
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-10-17 01:18:02 -0700

    Implement book model and routes; comment out hello world routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from .routes.hello_world_routes import hello_world_bp
+# from .routes.hello_world_routes import hello_world_bp
+from .routes.book_routes import book_bp
 
 def create_app():
     app = Flask(__name__)
     # Register Blueprints here
-    app.register_blueprint(hello_world_bp)
+    # app.register_blueprint(hello_world_bp)
+    app.register_blueprint(book_bp)
 
     return app
---
+++ app/routes/book_routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+from app.models.book import books
+
+book_bp = Blueprint("book", __name__, url_prefix="/books")
+
+@book_bp.get("")
+def get_all_books():
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
